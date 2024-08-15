34814433c73e79d77fa18a7438dd6f63d06b8d02
Vu_HP <vu.le.bui.quoc@gmail.com>
2024-08-15 11:10:26 +0700

    add 07_Chart.py

---
+++ Beginner_Guide/07_Chart.py
@@ -0,0 +1,17 @@
+# importing Necessary Libraries
+import pandas as pd
+import numpy as np
+import altair as alt
+import streamlit as st
+
+# Defining random Values for Chart
+df = pd.DataFrame(
+np.random.randn(10, 2),
+columns=['a', 'b'])
+
+# Defining Chart
+chart = alt.Chart(df).mark_bar().encode(
+x='a', y='b',  tooltip=['a', 'b'])
+
+# Defining Chart in write() function
+st.write(chart)

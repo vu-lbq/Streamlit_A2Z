e52c0c381c3d04940869f3ff350e80c4ddfa2558
Vu_HP <vu.le.bui.quoc@gmail.com>
2024-08-15 10:10:24 +0700

    add 05_Metric_Indicator

---
+++ Beginner_Guide/05_Metric_Indicator.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+#Defining Columns
+c1, c2, c3 = st.columns(3)
+
+# Defining Metrics
+c1.metric("Rainfall", "100 cm", "10 cm")
+c2.metric(label="Population", value="123 Billions", delta="1 Billions", delta_color="inverse")
+c3.metric(label="Customers", value=100, delta=10, delta_color="off")
+st.metric(label="Speed", value=None, delta=0)
+st.metric("Trees", "91456", "-1132649")

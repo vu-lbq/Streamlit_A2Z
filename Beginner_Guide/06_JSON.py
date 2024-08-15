5acc2a4c8c0aca7b277a4d1aa725de9d96a334ff
Vu_HP <vu.le.bui.quoc@gmail.com>
2024-08-15 10:18:46 +0700

    add O6_JSON

---
+++ Beginner_Guide/06_JSON.py
@@ -0,0 +1,23 @@
+import streamlit as st
+#Defining Nested JSON
+st.json
+(
+    { "Books" :
+        [{
+        "BookName" : "Python Testing with Selenium",
+        "BookID" : "1",
+        "Publisher" : "Apress",
+        "Year" : "2021",
+        "Edition" : "First",
+        "Language" : "Python",
+        },
+        {
+        "BookName": "Beginners Guide to Streamlit with Python",
+        "BookID" : "2",
+        "Publisher" : "Apress",
+        "Year" : "2022",
+        "Edition" : "First",
+        "Language" : "Python"
+        }]
+    }
+)
